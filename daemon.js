10a834ec42cf043d394cc39a85f46537d6de5b26
elf Pavlik <perpetual-tripper@wwelves.org>
2014-06-15 09:44:05 +0200

    refactored to promises and tidied up code

--- daemon.js
+++ daemon.js
@@ -1,41 +1,67 @@
 var fs = require('fs');
+var Promise = require('es6-promise').Promise;
 var express = require('express');
 var cors = require('cors');
 var config = require('./config.json');
 
 var daemon = express();
 
+/*
+ * CORS
+ *
+ * accepts cookies from foreign origin
+ * Pre-Flight https://github.com/troygoode/node-cors#enabling-cors-pre-flight
+ */
 daemon.use(cors({ origin: true, credentials: true }));
-
-// CORS Pre-Flight https://github.com/troygoode/node-cors#enabling-cors-pre-flight
 daemon.options('*', cors());
 
+
+// set view engine to handlebars
 daemon.set('view engine', 'hbs');
 
-function getJSON(nick){
-  //#FIXME make async!
-  return JSON.parse(fs.readFileSync(config.profilesDir + '/' + nick + '/index.json').toString());
+/*
+ * returns JSON representation of a profile document stored in config.profilesDir
+ * parameters
+ *   nick - path partial to profile data
+ *
+ */
+function getProfile(nick){
+  return new Promise(function(resolve, reject){
+    fs.readFile(config.profilesDir + '/' + nick + '/index.json', function(err, file){
+      if(err){
+        reject(err);
+      } else {
+        resolve(JSON.parse(file.toString()));
+      }
+    });
+  });
 }
 
+/*
+ * no favicon for now!
+ */
 daemon.get('/favicon.ico', function(req, res){
   res.send(404);
 });
 
+/*
+ * sends profile document
+ * supports content negotiation: RDFa and JSON-LD
+ */
 daemon.get('*', function(req, res){
   var nick = req.params[0].replace('/', '').toLowerCase();
-  var profile = getJSON(nick);
-  res.format({
-    'text/html': function(){
-      console.log(profile);
-      res.render('profile', profile);
-    },
-    // make JSON-LD
-    'application/json': function(){
-      res.json(profile);
-    },
-    'application/ld+json': function(){
-      res.json(profile);
-    }
+  getProfile(nick).then(function(data){
+    res.format({
+      'text/html': function(){
+        res.render('profile', data);
+      },
+      'application/json': function(){
+        res.json(data);
+      },
+      'application/ld+json': function(){
+        res.json(data);
+      }
+    });
   });
 });
 
